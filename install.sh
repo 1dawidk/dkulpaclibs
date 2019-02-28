853b7ce7bfe02a27980ae5b873c8060d5e5ce12a
Dawid Kulpa <1dawidk@gmail.com>
2019-02-28 09:47:58 +0100

    install script bug fixes

--- install.sh
+++ install.sh
@@ -8,47 +8,50 @@
 # sudo systemctl restart bluetooth
 # sudo chmod 777 /var/run/sdp
 
+echo "Install required dependencies"
+apt install libbluetooth-dev libasound2-dev libcurl4-openssl-dev libopencv-dev
+
 echo "Config blueZ..."
 #sed -i -e 's/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd --compat/g'
 #systemctl daemon-reload
 #systemctl restart bluetooth
 chmod 777 /var/run/sdp
 
-#Build misc library
-echo "Building dkulpaclibs..."
-cd ./dkulpamisc
-cmake --build /home/dkulpa/ToolBox/CLionProjects/dkulpaclibs/dkulpamisc/cmake-build-debug --target all -- -O -j 2
-cd ..
-
-cd ./dkulpahw
-cmake --build /home/dkulpa/ToolBox/CLionProjects/dkulpaclibs/dkulpahw/cmake-build-debug --target all -- -O -j 2
-cd ..
-
 #Create required dirs
 echo "Creating necessary dirs..."
 mkdir -p /usr/local/include/dkulpaclibs
-
 mkdir -p /usr/local/include/dkulpaclibs/misc
-
 mkdir -p /usr/local/include/dkulpaclibs/hw
-
-#Copy misc include files
-echo "Copy include files..."
-
-cd ./dkulpamisc
-rsync -av -m --include='*.h' --include='*/' --exclude='*' ./ /usr/local/include/dkulpaclibs/misc/
-cd ..
-
-cd ./dkulpahw
-rsync -av -m --include='*.h' --include='*/' --exclude='*' ./ /usr/local/include/dkulpaclibs/hw/
-cd ..
 
 #Copy find file
 echo "Copy cmake Find file..."
 cp ./Finddkulpaclibs.cmake /usr/local/lib/cmake/Finddkulpaclibs.cmake
+cp ./Finddkulpaclibs.cmake /usr/share/cmake-3.7/Modules/Finddkulpaclibs.cmake
 cp ./Finddkulpaclibs.cmake /home/dkulpa/ToolBox/CLion/bin/cmake/linux/share/cmake-3.13/Modules/Finddkulpaclibs.cmake
+
+#Build misc library
+echo "Building dkulpaclibs misc..."
+mkdir ./dkulpamisc/build
+cd ./dkulpamisc/build
+cmake ..
+make
+cd ..
+
+rsync -av -m --include='*.h' --include='*/' --exclude='*' ./ /usr/local/include/dkulpaclibs/misc/
+cd ..
+
+#Build hw library
+echo "Building dkulpaclibs hw..."
+mkdir ./dkulpahw/build
+cd ./dkulpahw/build
+cmake ..
+make
+cd ..
+
+rsync -av -m --include='*.h' --include='*/' --exclude='*' ./ /usr/local/include/dkulpaclibs/hw/
+cd ..
 
 #Copy misc bin
 echo "Copy binary files..."
-cp ./dkulpamisc/cmake-build-debug/libdkulpamisc.a /usr/local/lib/libdkulpaclibs_misc.a
-cp ./dkulpahw/cmake-build-debug/libdkulpahw.a /usr/local/lib/libdkulpaclibs_hw.a
+cp ./dkulpamisc/build/libdkulpamisc.a /usr/local/lib/libdkulpaclibs_misc.a
+cp ./dkulpahw/build/libdkulpahw.a /usr/local/lib/libdkulpaclibs_hw.a
